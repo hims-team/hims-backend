ec5c114caff1d409b879731ecf438e8fa6d2c020
ajimae <meeky.ae@gmail.com>
2019-05-01 01:12:56 +0100

    chore(testing): setup testing framework
    - install all tests suites including mocha, chai, chai-http
    - install nyc coverage reporter
    - include nyc configuration in package.json
    - include a dummy test file
    
    [Finishes #165229448]

--- src/db/config/config.ts
+++ src/db/config/config.ts
@@ -7,6 +7,7 @@ const config = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
+    operatorsAliases: false,
     dialect: 'postgres',
     logging: false,
   },
